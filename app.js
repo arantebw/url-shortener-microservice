e942025a8c44881617d350661c82b9b960a417f1
Billy Arante <arantebillywilson@gmail.com>
2019-07-16 15:41:51 +0800

    add: Load assets and Font Awesome

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // app.js
 
 'use strict';
+const cors = require('cors');
 const sass = require('node-sass-middleware');
 const express = require('express');
 
 const app = express();
 
-// Load assets
-app.use(express.static(__dirname + '/public'));
+// Middlewares
+
+// CORS
+app.use(cors({ optionSuccessStatus: 200 }));
 
 // Sass
 app.use(sass({
-    'src': __dirname + '/assets',
-    'dest': __dirname + 'public',
-    'debug': true,
-    
+    src: __dirname + '/assets',
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'compressed'
 }));
+
+// Font Awesome
+app.use('/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
+
+// Load assets
+app.use(express.static(__dirname + '/public'));
 
 // Routes
 app.get('/', (req, res) => {
